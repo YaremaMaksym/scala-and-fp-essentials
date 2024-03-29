283d13f7af75f7bd7f7bb19b294cd0ccc4ab2058
Maksym Yarema <macsym2@gmail.com>
2024-03-29 18:04:46 +0200

    complete MethodNotations (exercises)

--- src/main/scala/lectures/part2oop/MethodNotations.scala
+++ src/main/scala/lectures/part2oop/MethodNotations.scala
@@ -4,12 +4,18 @@ import scala.language.postfixOps
 
 object MethodNotations extends App {
 
-  class Person(val name: String, favoriteMovie: String) {
+  class Person(val name: String, favoriteMovie: String, val age: Int = 0) {
     def likes(movie: String): Boolean = movie == favoriteMovie
-    def hangOutWith(person: Person): String = s"${this.name} hangs out with ${person.name}"
+    def +(person: Person): String = s"${this.name} hangs out with ${person.name}"
     def unary_! : String = s"$name, wht!?"
     def isAlive : Boolean = true
     def apply(): String = s"Hi, my name is $name and I like $favoriteMovie"
+
+    def +(info: String): Person = new Person(this.name + s" ($info)", this.favoriteMovie)
+    def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
+    def learns(thing: String) = s"$name learns $thing"
+    def learnsScala: String = learns("Scala")
+    def apply(n: Int) = s"$name watched $favoriteMovie $n times"
   }
 
   val mary = new Person("Mary", "Inception")
@@ -19,7 +25,7 @@ object MethodNotations extends App {
 
   // "operators" in Scala
   val tom = new Person("Tom", "Fight Club")
-  println(mary hangOutWith tom)
+  println(mary + tom)
 
   println(s"1+2 = ${1 + 2}")
   println(s"1.+(2) = ${1.+(2)}")
@@ -48,4 +54,15 @@ object MethodNotations extends App {
    */
   println(mary.apply())
   println(mary())
+
+
+
+
+  println((mary + "the rockstar").name)
+  println((+mary).age)
+  println(mary learns "thing")
+  println(mary learnsScala)
+  println(mary(4))
+
+
 }
