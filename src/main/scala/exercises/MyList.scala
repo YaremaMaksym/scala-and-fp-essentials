cd6ba1a78080fe33678f3ec3b6020f340588983d
Maksym Yarema <macsym2@gmail.com>
2024-03-31 09:17:35 +0300

    complete Generics exercise

--- src/main/scala/exercises/MyList.scala
+++ src/main/scala/exercises/MyList.scala
@@ -1,4 +1,7 @@
 package exercises
+
+import scala.runtime.Nothing$
+
 /*
 abstract class MyList(val head: Node, val tail: Node) {
 
@@ -14,38 +17,42 @@ abstract class MyList(val head: Node, val tail: Node) {
 
 class Node(val number: Int, val nextNode: Node)*/
 
-abstract class MyList {
-  def head: Int
-  def tail: MyList
+abstract class MyList[+A] {
+  def head: A
+  def tail: MyList[A]
   def isEmpty: Boolean
-  def add(element: Int): MyList
+  def add[B >: A](element: B): MyList[B]
   def printElements: String
   // polymorphic call
   override def toString: String = "[" + printElements + "]"
 }
 
-object Empty extends MyList {
-  def head: Int = throw new NoSuchElementException
-  def tail: MyList = throw new NoSuchElementException
+object Empty extends MyList[Nothing] {
+  def head: Nothing = throw new NoSuchElementException
+  def tail: MyList[Nothing] = throw new NoSuchElementException
   def isEmpty: Boolean = true
-  def add(element: Int): MyList = new Cons(element, Empty)
+  def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
   def printElements: String = ""
 }
 
-class Cons(h: Int, t: MyList) extends MyList:
-  override def head: Int = h
-  override def tail: MyList = t
+class Cons[+A](h: A, t: MyList[A]) extends MyList[A]:
+  override def head: A = h
+  override def tail: MyList[A] = t
   override def isEmpty: Boolean = false
-  override def add(element: Int): MyList = new Cons(element, this)
+  override def add[B >: A](element: B): MyList[B] = new Cons(element, this)
   def printElements: String =
     if (t.isEmpty) "" + h
     else s"$h ${t.printElements}"
 
-
 object ListTest extends App {
-  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
-  println(s"list.head = ${list.tail.head}")
-  println(s"list.add(5).head = ${list.add(5).head}")
-  println(s"list.isEmpty = ${list.isEmpty}")
-  println(s"list = ${list}")
+//  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
+//  println(s"list.head = ${list.tail.head}")
+//  println(s"list.add(5).head = ${list.add(5).head}")
+//  println(s"list.isEmpty = ${list.isEmpty}")
+//  println(s"list = ${list}")
+  val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val listOfStrings: MyList[String] = new Cons("Hello", new Cons("Scala", Empty))
+
+  println(s"listOfIntegers = ${listOfIntegers}")
+  println(s"listOfStrings = ${listOfStrings}")
 }
