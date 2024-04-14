8a8ed12fbd0b8a00addc2c62e796f628e48b82ee
Maksym Yarema <macsym2@gmail.com>
2024-04-14 14:47:39 +0300

    complete TuplesAndMaps (exercise)

--- src/main/scala/lectures/part3fp/TuplesAndMaps.scala
+++ src/main/scala/lectures/part3fp/TuplesAndMaps.scala
@@ -13,7 +13,7 @@ object TuplesAndMaps extends App {
   // Maps - key -> value
   val aMap: Map[String, Int] = Map()
 
-  val phonebook = Map(("Jim", 555), "Daniel" -> 789).withDefaultValue(-1)
+  val phonebook = Map(("Jim", 555), "Daniel" -> 789, "JIM" -> 9000).withDefaultValue(-1)
   println(phonebook)
 
   // map ops
@@ -39,4 +39,94 @@ object TuplesAndMaps extends App {
 
   val names = List("Bob", "James", "Angela", "Mary", "Daniel", "Jim")
   println(names.groupBy(name => name.charAt(0))) // used often in Spark
+
+  /*
+    1. Overly simplified social network based on maps
+        Person = String
+        - add person to the network
+        - remove
+        - friend (mutual)
+        - unfriend
+
+        - number of friends of a person
+        - person with most friends
+        - how many people have NO friends
+        - if there is social connection between two people (direct or not)
+   */
+
+  def add(network: Map[String, Set[String]], newPerson: String): Map[String, Set[String]] =
+    network + (newPerson -> Set())
+
+  def addConnection(network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
+    val friendsA = network(a)
+    val friendsB = network(b)
+
+    network + (a -> (friendsA + b)) + (b -> (friendsB + a))
+  }
+
+  def removeConnection(network: Map[String, Set[String]], a: String, b: String): Map[String, Set[String]] = {
+    val friendsA = network(a)
+    val friendsB = network(b)
+
+    network + (a -> (friendsA - b)) + (b -> (friendsB - a))
+  }
+
+  def remove(network: Map[String, Set[String]], person: String): Map[String, Set[String]] = {
+    def removeAux(friends: Set[String], networkAcc: Map[String, Set[String]]): Map[String, Set[String]] = {
+      if (friends.isEmpty) networkAcc
+      else removeAux(friends.tail, removeConnection(networkAcc, person, friends.head))
+    }
+    val withRemovedConnection = removeAux(network(person), network)
+    withRemovedConnection - person
+  }
+
+  val empty: Map[String, Set[String]] = Map()
+  val network = add(add(empty, "Mary"), "Bob")
+  println(network)
+  println(addConnection(network, "Bob", "Mary"))
+  println(removeConnection(addConnection(network, "Bob", "Mary"), "Bob", "Mary"))
+  println(remove(addConnection(network, "Bob", "Mary"), "Bob"))
+
+
+  val people = add(add(add(empty, "Mary"), "Bob"), "Jim")
+  val jimBob = addConnection(people, "Bob", "Jim")
+  val testNetwork = addConnection(jimBob, "Bob", "Mary")
+
+  println(testNetwork)
+
+  def nFriends(network: Map[String, Set[String]], person: String): Int = {
+    if (!network.contains(person)) 0
+    else network(person).size
+  }
+
+  println(nFriends(testNetwork, "Bob"))
+
+  def mostFriends(network: Map[String, Set[String]]): String = {
+    network.maxBy(pair => pair._2.size)._1
+  }
+
+  println(mostFriends(testNetwork))
+
+  def nPeopleWithNoFriends(network: Map[String, Set[String]]): Int = {
+    network.count(pair => pair._2.isEmpty)
+  }
+
+  println(nPeopleWithNoFriends(jimBob))
+
+  def isConnected(network: Map[String, Set[String]], a: String, b: String): Boolean = {
+    def bfs(target: String, consideredPeople: Set[String], discoveredPeople: Set[String]): Boolean = {
+      if (discoveredPeople.isEmpty) false
+      else {
+        val person = discoveredPeople.head
+        if (person == target) true
+        else if (consideredPeople.contains(person)) bfs(target, consideredPeople, discoveredPeople.tail)
+        else bfs(target, consideredPeople + person, discoveredPeople.tail ++ network(person))
+      }
+    }
+
+    bfs(b, Set(), network(a) + a)
+  }
+
+  println(isConnected(testNetwork, "Jim", "Mary"))
+  println(isConnected(jimBob, "Jim", "Mary"))
 }
