d874a05992e7cba7a15c61f19f33ab292326b142
Maksym Yarema <macsym2@gmail.com>
2024-04-20 10:14:05 +0300

    complete Pattern Matching (exercise)

--- src/main/scala/lectures/part3fp/part4/PatternMatching.scala
+++ src/main/scala/lectures/part3fp/part4/PatternMatching.scala
@@ -22,6 +22,7 @@ object PatternMatching extends App {
     1. Decompose values
    */
   case class Person(name: String, age: Int)
+
   val bob = Person("Bob", 20)
 
   val greeting = bob match
@@ -29,7 +30,7 @@ object PatternMatching extends App {
     case Person(n, a) => s"Hi, my name is $n and I'm $a years old"
     case _ => "Hello"
 
-  println(greeting )
+  println(greeting)
 
   /*
    - cases are matched in order
@@ -40,7 +41,9 @@ object PatternMatching extends App {
 
   // PM on sealed hierarchies
   sealed class Animal
+
   case class Dog(breed: String) extends Animal
+
   case class Parrot(greeting: String) extends Animal
 
   val animal: Animal = Dog("Terra Nova")
@@ -52,8 +55,39 @@ object PatternMatching extends App {
   val isEven = x match
     case n if n % 2 == 0 => true
     case _ => false
-//      overkill
+  //      overkill
 
   val isEvenCond = if (x % 2 == 0) true else false // also bad
   val isEvenNormal = x % 2 == 0
+
+  /*
+    Exercise
+    simple function uses PM
+      takes an Expr => human readable form
+   */
+  trait Expr
+
+  case class Number(n: Int) extends Expr
+
+  case class Sum(e1: Expr, e2: Expr) extends Expr
+
+  case class Prod(e1: Expr, e2: Expr) extends Expr
+
+  def show(e: Expr): String = e match
+    case Number(n) => s"$n"
+    case Sum(e1, e2) => show(e1) + " + " + show(e2)
+    case Prod(e1, e2) => {
+      def maybeShowParentheses(exp: Expr) = exp match
+        case Prod(_, _) => show(exp)
+        case Number(_) => show(exp)
+        case _ => "(" + show(exp) + ")"
+
+      maybeShowParentheses(e1) + " * " + maybeShowParentheses(e2)
+    }
+
+  println()
+  println(show(Sum(Number(2), Number(3))))
+  println(show(Sum(Sum(Number(3), Number(2)), Number(3))))
+  println(show(Prod(Sum(Number(2), Number(1)), Number(3))))
+  println(show(Sum(Prod(Number(2), Number(1)), Number(3))))
 }
